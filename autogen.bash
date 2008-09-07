c6c756ca53179e540343bbf1f707dbe797d330bf
Fernando J. Pereda <ferdy@ferdyx.org>
2008-09-07 21:06:47 +0200

    OSX stuff
    Signed-off-by: Fernando J. Pereda <ferdy@ferdyx.org>

--- autogen.bash
+++ autogen.bash
@@ -17,22 +17,33 @@ run() {
 }
 
 get() {
-	local p=${1} v=
+	local n=${1} v=
 	shift
 
-	for v in ${@} ; do
-		type ${p}-${v}    &>/dev/null && echo ${p}-${v}   && return
-		type ${p}${v//.}  &>/dev/null && echo ${p}${v//.} && return
+	for p in ${n} g${n} ; do
+		for v in ${@} ; do
+			type ${p}-${v}    &>/dev/null && echo ${p}-${v}   && return
+			type ${p}${v//.}  &>/dev/null && echo ${p}${v//.} && return
+		done
+		type ${p}             &>/dev/null && echo ${p}        && return
 	done
-	type ${p}             &>/dev/null && echo ${p}        && return
-	echo "Could not find ${p}" 1>&2
+	echo "Could not find ${n}" 1>&2
 	kill $KILL_PID
 }
 
 run mkdir -p config
 run $(get libtoolize 1.5 ) --copy --force --automake
 rm -f config.cache
-run $(get aclocal 1.10 1.9 )
+
+case $(uname -s) in
+	Darwin)
+		run $(get aclocal 1.10 1.9 ) -I /opt/local/share/aclocal
+	;;
+	*)
+		run $(get aclocal 1.10 1.9 )
+	;;
+esac
+
 run $(get autoheader 2.61 2.60 2.59 )
 run $(get autoconf 2.61 2.60 2.59 )
 run $(get automake 1.10 1.9 ) -a --copy --foreign
